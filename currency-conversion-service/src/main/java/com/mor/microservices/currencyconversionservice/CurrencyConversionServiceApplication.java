28dc53ff573aafd283a1fb15165e614dfe01b808
morawat <morawat@MORAWAT-IN.oradev.oraclecorp.com>
2021-10-27 20:45:24 +0530

    First commit of curency conversion service

---
+++ currency-conversion-service/src/main/java/com/mor/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,18 @@
+package com.mor.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/*
+ * @EnableFeignClients: This allows scanning for interfaces that are declared as feign clients.
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ currency-conversion-service/src/main/java/com/mor/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.mor.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/*
+ * @FeignClient: Uses the argument as the client’s name during load balancing.
+ */
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+
+}
