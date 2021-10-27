23cf169d9bd5b24217d5f76e3376c39e672dbcd6
morawat <morawat@MORAWAT-IN.oradev.oraclecorp.com>
2021-10-27 20:46:49 +0530

    First commit of naming server (Eureka)

---
+++ naming-server/src/main/java/com/mor/microservices/namingserver/NamingServerApplication.java
@@ -0,0 +1,18 @@
+package com.mor.microservices.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+ * EnableEurekaServer: makes your Spring Boot application acts as a Eureka Server.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+
+}
